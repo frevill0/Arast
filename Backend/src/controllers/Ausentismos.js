295d63c3c830b081887eb09b11623c3850db5514
Pasante Tecnología <pasante_tecnologia@qtgc.com>
2024-09-05 11:33:08 -0500

    cambios del backend

--- Backend/src/controllers/Ausentismos.js
+++ Backend/src/controllers/Ausentismos.js
@@ -12,50 +12,46 @@ const formatoFecha = (dateString) => {
 }
 
 const ConsultaAusentismo = async (req, res) => {
-
   const membresia = req.params.Membresia;
   console.log("Membresía recibida:", membresia);
-
 
   if (!membresia) {
     return res.status(400).send({ msg: "Perdón, pero no existe un socio con esa membresía" });
   }
 
   try {
-    
-    const encontrarSocio = await prisma.contactscm_fac_elec_arast.findFirst({
+    const encontrarSocio = await prisma.contactscm_fac_elec_arast.findUnique({
       where: {
-        Membresia : membresia,
+        Membresia: membresia,
       },
-      
     });
+    console.log("fecha suspension:", encontrarSocio.fechaRetSep);
 
     if (!encontrarSocio) {
-        return res.status(404).send({ msg: "No se encontró un socio con esa membresía" });
-      }
+      return res.status(404).send({ msg: "No se encontró un socio con esa membresía" });
+    }
 
-      const estadosAusentes = ['Ausente', 'Ausente > 26', 'Ausente > 27', 'Retirado'];
-      if (!estadosAusentes.includes(encontrarSocio.Estatus)) {
-        console.log("Estatus:", encontrarSocio.Estatus);
-        return res.status(404).send({ msg: "El socio no se encuentra ausente" });
-      }
+    const estadosAusentes = ['Ausente', 'Ausente > 26', 'Ausente > 27', 'Retirado'];
+    if (!estadosAusentes.includes(encontrarSocio.Estatus)) {
+      console.log("Estatus:", encontrarSocio.Estatus);
+      return res.status(404).send({ msg: "El socio no se encuentra ausente" });
+    }
 
-      const resultado = {
-        Socio: encontrarSocio.Socio,
-        Categoria: encontrarSocio.Categoria,
-        Estatus: encontrarSocio.Estatus,
-        Titular: encontrarSocio.Titular,
-        FechaNacimiento: formatoFecha(encontrarSocio.FechaNac),
-        Edad: encontrarSocio.Edad,
-        FechaAusentismo: formatoFecha(encontrarSocio.Marca_Ausentes),
-      }
+    const resultado = {
+      Socio: encontrarSocio.Socio,
+      Categoria: encontrarSocio.Categoria,
+      Estatus: encontrarSocio.Estatus,
+      Titular: encontrarSocio.Titular,
+      FechaNacimiento: formatoFecha(encontrarSocio.FechaNac),
+      Edad: encontrarSocio.Edad,
+      FechaAusentismo: encontrarSocio.fechaRetSep,
+    }
 
     res.status(200).json({ res: 'Detalles del socio: ', data: resultado });
   } catch (error) {
     console.error(error);
     res.status(500).send({ msg: "Error interno del servidor" });
   }
-
 };
 
 
@@ -63,9 +59,6 @@ export const RegistroMigratorio = () => {
 
   
 }
-
-
-
 
 export {
   ConsultaAusentismo,
